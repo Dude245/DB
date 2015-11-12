d75617618e996aaf74aa27d5ede7f90be614d56f
Nathan <Nathan@Nathans-MacBook-Pro.local>
2015-11-12 15:21:17 -0500

    Boo

--- a3data.sql
+++ a3data.sql
@@ -24,8 +24,8 @@
 
 
 -- insert into V values (’V1’, ’Sears’, ’Toronto', 200.00);
--- insert into V values (’V2’, 'Walmart', ’Waterloo', 6);71.05
--- insert into V values (’V3’, 'Esso', ’Windsor', 0);
+-- insert into V values (’V2’, 'Walmart', ’Waterloo', 671.05);
+-- insert into V values (’V3’, 'Esso', ’Windsor', 0.00);
 -- insert into V values (’V4’, ’Esso’, ’Waterloo', 225.00);
 
 
@@ -34,7 +34,7 @@
 -- insert into C values (’A3’, ’Doc’, ’ONT’, 1000.00, 1000);
 
 -- insert into T values (’T1’, ’V2’, ’A1', 2015-07-15, 1325.00);
--- insert into T values (’T2’, 'V2', ’A3', 2014-12-16, 1900.00)
+-- insert into T values (’T2’, 'V2', ’A3', 2014-12-16, 1900.00);
 -- insert into T values (’T3’, 'V3', ’A1', 2015-09-01, 2500.00);
 -- insert into T values (’T4’, ’V4’, ’A2', 2015-03-20, 1613.00);
 -- insert into T values (’T5’, ’V4’, ’A3', 2015-07-31, 3312.00);
